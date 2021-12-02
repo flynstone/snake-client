ee54d0461c05c41edbf1056f3262c1751a9fc97b
Julien <flynstone@live.ca>
2021-12-01 22:57:56 -0500

    update snake-client

--- input.js
+++ input.js
@@ -1,24 +1,41 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
 
   stdin.on("data", key => {
-    process.stdout.write(handleUserInput(key));
+    let output = handleUserInput(key);
+    process.stdout.write(output);
   });
   return stdin;
-};
-
+}
 
 const handleUserInput = key => {
-  const stdout = process.stdout;
-  //stdout.write(".");
+
   if (key === "\u0003") {
     process.exit();
   }
 
+  if (key === "w") {
+    connection.write("Move: up");
+  }
+
+  if (key === "a") {
+    connection.write("Move: left");
+  }
+
+  if (key === "s") {
+    connection.write("Move: down");
+  }
+
+  if (key === "d") {
+    connection.write("Move: right");
+  }
+
   return key;
 };
-
-module.exports = { setupInput, handleUserInput };
+module.exports = { setupInput };
